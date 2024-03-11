866e1c74e9bbcb5ee9f4bfa2c353aad6edbba4b1
chongweiwu <130844098+chongweiwu@users.noreply.github.com>
2024-03-11 17:41:24 +0800

    Add files via upload

---
+++ code/dataAug.py
@@ -0,0 +1,72 @@
+import math
+import torch
+import random
+import numpy as np
+import torch.nn.functional as F
+
+def RandomGammaCorrection(img):
+    lower = 0.8
+    upper = 1.2
+        
+    factor = random.choice(np.arange(lower, upper, 0.05))
+    gamma  = torch.tensor(factor)
+    img    = img ** gamma
+    return img
+
+def affine_aug(im, im_label=None):
+    # mode = 'bilinear' or 'nearest'
+    with torch.no_grad():
+        angle_range = 10
+        trans_range = 0.05
+        scale_range = 0.0
+        # scale_range = 0.15
+
+        angle_xyz = (random.uniform(-angle_range, angle_range) * math.pi / 180,
+                     random.uniform(-angle_range, angle_range) * math.pi / 180,
+                     random.uniform(-angle_range, angle_range) * math.pi / 180)
+        scale_xyz = (random.uniform(-scale_range, scale_range), random.uniform(-scale_range, scale_range),
+                     random.uniform(-scale_range, scale_range))
+        trans_xyz = (random.uniform(-trans_range, trans_range), random.uniform(-trans_range, trans_range),
+                     random.uniform(-trans_range, trans_range))
+
+        rotation_x = torch.tensor([
+            [1., 0, 0, 0],
+            [0, math.cos(angle_xyz[0]), -math.sin(angle_xyz[0]), 0],
+            [0, math.sin(angle_xyz[0]), math.cos(angle_xyz[0]), 0],
+            [0, 0, 0, 1.]
+        ], requires_grad=False).unsqueeze(0).cuda()
+
+        rotation_y = torch.tensor([
+            [math.cos(angle_xyz[1]), 0, math.sin(angle_xyz[1]), 0],
+            [0, 1., 0, 0],
+            [-math.sin(angle_xyz[1]), 0, math.cos(angle_xyz[1]), 0],
+            [0, 0, 0, 1.]
+        ], requires_grad=False).unsqueeze(0).cuda()
+
+        rotation_z = torch.tensor([
+            [math.cos(angle_xyz[2]), -math.sin(angle_xyz[2]), 0, 0],
+            [math.sin(angle_xyz[2]), math.cos(angle_xyz[2]), 0, 0],
+            [0, 0, 1., 0],
+            [0, 0, 0, 1.]
+        ], requires_grad=False).unsqueeze(0).cuda()
+
+        trans_shear_xyz = torch.tensor([
+            [1. + scale_xyz[0], 0, 0, trans_xyz[0]],
+            [0, 1. + scale_xyz[1], 0, trans_xyz[1]],
+            [0, 0, 1. + scale_xyz[2], trans_xyz[2]],
+            [0, 0, 0, 1]
+        ], requires_grad=False).unsqueeze(0).cuda()
+
+        theta_final = torch.matmul(rotation_x, rotation_y)
+        theta_final = torch.matmul(theta_final, rotation_z)
+        theta_final = torch.matmul(theta_final, trans_shear_xyz)
+
+        output_disp_e0_v = F.affine_grid(theta_final[:, 0:3, :], im.shape, align_corners=True)
+        im = F.grid_sample(im, output_disp_e0_v, mode='bilinear', padding_mode="border", align_corners=True)
+
+        if im_label is not None:
+            im_label = F.grid_sample(im_label, output_disp_e0_v, mode='bilinear', padding_mode="border", align_corners=True)
+            return im, im_label
+        else:
+            return im
+
